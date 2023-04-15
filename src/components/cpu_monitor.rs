bb48112b701c1b2152e52649089a07f69feed9e8
JosueRhea <josueroyale8171@gmail.com>
2023-04-15 04:32:14 +0000

    Show memory usage

--- src/components/cpu_monitor.rs
+++ src/components/cpu_monitor.rs
@@ -12,13 +12,16 @@ pub fn cpu_monitor(state: &mut State) -> List {
     let brand = state.system.global_cpu_info().brand();
     let usage = state.system.global_cpu_info().cpu_usage();
     let cpus = state.system.cpus();
+    let memory_available = state.system.available_memory() as f64 / 1024_f64.powi(3);
+    let total_memory = state.system.total_memory() as f64 / 1024_f64.powi(3);
 
     // let comp = state.system.components().get(0).unwrap();
 
     let texts = vec![
-        format!("Usage: {}%", usage.ceil()),
+        format!("CPU: {}%", usage.ceil()),
         format!("Brand: {}", brand),
         format!("Cores: {}", cpus.len()),
+        format!("Memory: {:.2} of {:.2}", memory_available, total_memory),
     ];
 
     let spans = (0..texts.len()).map(|i| {
